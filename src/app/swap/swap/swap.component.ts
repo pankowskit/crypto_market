63636d25f1d4a759c0c174156ace0f05cee0f5b9
pankowskitomasz <pankowski.email@gmail.com>
2022-02-27 20:00:57 +0200

    routing

---
+++ src/app/swap/swap/swap.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-swap',
+  templateUrl: './swap.component.html',
+  styleUrls: [
+    './swap.component.scss',
+    "../../sass/styles.scss"
+  ]
+})
+export class SwapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
